038b240b0aaa6d8fda0d387d3d1bd45442fe1637
Emili-code <72404504+Emili-code@users.noreply.github.com>
2020-10-08 15:12:32 +0200

    Add files via upload

---
+++ ex08/src/index.js
@@ -0,0 +1,21 @@
+function main ( numOne , numTwo ) {
+    // Only change code below this line
+var numOne;
+var numTwo;
+var sum;
+
+sum = 15 + 8;
+console.log(sum(15, 8))
+
+sum = 5 + 28;
+console.log(sum(5, 28))
+
+sum = 150 + 425; 
+console.log(sum(150, 425))
+
+
+    // Only change code above this line
+    return sum;
+    }
+    console . log ( main ( numOne , numTwo )); // change this line
+    module . exports = main;
